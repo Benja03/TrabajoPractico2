e97c8f4742fc740187dd54f9606644002d6c24ac
Benjamin Berterreix <benja.berterreix@gmail.com>
2020-06-08 22:23:51 -0300

    Adición de la segunda activity

---
+++ app/src/main/java/com/example/trabajopractico2/SegundaActivity.java
@@ -0,0 +1,14 @@
+package com.example.trabajopractico2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
